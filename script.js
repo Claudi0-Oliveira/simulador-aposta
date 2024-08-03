0e2c6947f563ed33c335b4af6c6c101c4871d5ed
Claudio Oliveira <claudio@cjservices.com.br>
2024-08-03 14:14:08 -0300

    add function for comparation numbers

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-// Função para gerar um número aleatório
+// Função para gerar um número aleatório entre 0 e 99
 function gerarNumeroAleatorio() {
     return Math.floor(Math.random() * 100);
 }
 
-// Função para exibir o número sorteado
-function mostrarNumero() {
-    const numero = gerarNumeroAleatorio();
-    document.getElementById('resultado').textContent = numero;
+// Função para exibir o resultado do sorteio
+function mostrarResultado() {
+    const numeroUsuario = parseInt(document.getElementById('numeroUsuario').value);
+    const numeroSorteado = gerarNumeroAleatorio();
+
+    // Verifica se o valor inserido pelo usuário é válido
+    if (isNaN(numeroUsuario) || numeroUsuario < 0 || numeroUsuario > 99) {
+        alert('Por favor, insira um número válido entre 0 e 99.');
+        return;
+    }
+
+    // Compara o número do usuário com o número sorteado
+    if (numeroUsuario === numeroSorteado) {
+        document.getElementById('resultado').innerHTML = `Parabéns!<br>Você acertou o número ${numeroSorteado} e ganhou o prêmio!`;
+    } else {
+        document.getElementById('resultado').innerHTML = `Não foi dessa vez.<br><br>O número sorteado foi:<br><br> ${numeroSorteado}.<br><br>Tente novamente!`;
+    }
 }
 
 // Adiciona um ouvinte de eventos ao botão
-document.getElementById('sortearBtn').addEventListener('click', mostrarNumero);
+document.getElementById('sortearBtn').addEventListener('click', mostrarResultado);
